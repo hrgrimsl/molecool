46aebd08787f77d2d7500139bb80eaac417175c1
Harper <hrgrimsl@vt.edu>
2022-05-02 13:40:41 -0400

    add open_pdb function to molecool

--- molecool/functions.py
+++ molecool/functions.py
@@ -1,4 +1,40 @@
 """Provide the primary functions."""
+import numpy as np
+
+def write_xyz(file_location, symbols, coordinates):
+    
+    # Write an xyz file given a file location, symbols, and coordinates.
+    num_atoms = len(symbols)
+    
+    if num_atoms != len(coordinates):
+        raise ValueError(f"write_xyz : the number of symbols ({num_atoms}) and number of coordinates ({len(coordinates)}) must be the same to write xyz file!")
+    
+    with open(file_location, 'w+') as f:
+        f.write('{}\n'.format(num_atoms))
+        f.write('XYZ file\n')
+        
+        for i in range(num_atoms):
+            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i], 
+                                              coordinates[i,0], coordinates[i,1], coordinates[i,2]))
+
+
+def open_pdb(file_location):
+    
+    with open(file_location) as f:
+        data = f.readlines()
+
+    coordinates = []
+    symbols = []
+    for line in data:
+        if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
+            symbols.append(line[76:79].strip())
+            atom_coords = [float(x) for x in line[30:55].split()]
+            coordinates.append(atom_coords)
+
+    coords = np.array(coordinates)
+    symbols = np.array(symbols)
+
+    return symbols, coords
 
 def zen(with_attribution=True):
     quote = """Beautiful is better than ugly.
